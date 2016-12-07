24a857545ce7aca1f7701d50b00370aac68ca98d
Alex Arnell <alex@heroku.com>
2016-12-07 00:36:56 -0800

    parses and stores logs in redis list

--- cmd/log-boom/server.go
+++ cmd/log-boom/server.go
@@ -1,6 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"errors"
+	"fmt"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
@@ -18,7 +22,10 @@ type env struct {
 }
 
 func (e *env) healthHandler(w http.ResponseWriter, r *http.Request) {
-	status := http.StatusNoContent
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
 
 	ok, err := e.db.Healthcheck()
 	if err != nil {
@@ -26,24 +33,128 @@ func (e *env) healthHandler(w http.ResponseWriter, r *http.Request) {
 			"at":  "healthcheck",
 			"err": err,
 		}).Error("unable to healthcheck datastore")
-		status = http.StatusServiceUnavailable
+
+		http.Error(w, http.StatusText(503), 503)
+		return
 	}
+
 	if !ok {
 		log.WithFields(log.Fields{
 			"at": "healthcheck",
 		}).Error("healthcheck failed")
-		status = http.StatusServiceUnavailable
+		http.Error(w, http.StatusText(503), 503)
+		return
 	}
 
-	log.WithFields(log.Fields{
-		"at": "healthcheck",
-		"ok": ok,
-	}).Debug("health checked")
-	w.WriteHeader(status)
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(204)
 }
 
 func (e *env) logsHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNoContent)
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), 405)
+		return
+	}
+	if r.Header.Get("Content-Type") != "application/logplex-1" {
+		http.Error(w, http.StatusText(415), 415)
+		return
+	}
+	token := r.Header.Get("Logplex-Drain-Token")
+	count, err := strconv.ParseInt(r.Header.Get("Logplex-Msg-Count"), 10, 32)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"at":  "logs",
+			"err": err,
+		}).Error("unable to parse Logplex-Msg-Count header")
+		http.Error(w, http.StatusText(400), 400)
+	}
+
+	lines, err := process(r.Body, count)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"at":  "logs",
+			"err": err,
+		}).Error("could not process body")
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+	stored, err := e.db.Insert(token, lines)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"at":  "logs",
+			"err": err,
+		}).Error("could not store logs")
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	log.WithFields(log.Fields{
+		"at":     "logs",
+		"len":    len(lines),
+		"stored": stored,
+	}).Info("stored logs in redis")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
+	w.WriteHeader(204)
+}
+
+const (
+	// MinSyslogFrameSize is the smallest (all NULLVALUE) size a syslog frame can be.
+	MinSyslogFrameSize = 18
+
+	// MaxSyslogFrameSize is the largest size a syslog frame can be.
+	MaxSyslogFrameSize = 10 * 1024
+)
+
+func scanOffset(data []byte, atEOF bool) (int, error) {
+	advance, token, err := bufio.ScanWords(data, atEOF)
+	if token == nil || err != nil {
+		return 0, err
+	}
+
+	offset, err := strconv.ParseInt(string(token), 10, 32)
+	if err != nil {
+		return 0, err
+	}
+	return advance + int(offset), nil
+}
+
+func readSyslogFrame(data []byte, atEOF bool) (int, []byte, error) {
+
+	advance, err := scanOffset(data, atEOF)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	if advance < MinSyslogFrameSize || advance > MaxSyslogFrameSize {
+		return 0, nil, errors.New("Invalid Syslog Frame")
+	}
+
+	if !atEOF && advance > len(data) {
+		return 0, nil, nil
+	}
+
+	if atEOF && len(data)-advance < MinSyslogFrameSize {
+		return advance, data[:advance], bufio.ErrFinalToken
+	}
+
+	return advance, data[:advance], nil
+}
+
+func process(r io.Reader, count int64) ([]string, error) {
+	scanner := bufio.NewScanner(r)
+	scanner.Split(readSyslogFrame)
+
+	lines := make([]string, 0, count)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+		fmt.Printf("%#v\n", lines)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
 }
 
 func main() {
@@ -56,11 +167,16 @@ func main() {
 	if err != nil || url.Scheme != "redis" {
 		log.Fatal("$REDIS_URL must be set and valid")
 	}
+	keep, err := strconv.Atoi(os.Getenv("BUFFER_SIZE"))
+	if err != nil {
+		keep = 1500
+	}
 	size, err := strconv.Atoi(os.Getenv("REDIS_POOL_SIZE"))
 	if err != nil {
 		size = DefaultRedisPoolSize
 	}
-	db, err := ds.NewRedisBackend(url, size)
+
+	db, err := ds.NewRedisBackend(url, keep, size)
 	if err != nil {
 		log.Fatal(err)
 	}
--- datastore/redis.go
+++ datastore/redis.go
@@ -3,6 +3,7 @@ package datastore
 import (
 	"net/url"
 
+	log "github.com/Sirupsen/logrus"
 	"github.com/mediocregopher/radix.v2/pool"
 	"github.com/mediocregopher/radix.v2/redis"
 )
@@ -13,13 +14,9 @@ type Datastore interface {
 	HealthChecker
 }
 
-// Batch is a batch of logs to insert.
-type Batch struct {
-}
-
 // Inserter is the interface for inserting records into the Datastore.
 type Inserter interface {
-	Insert(batch Batch)
+	Insert(token string, lines []string) (int, error)
 }
 
 // HealthChecker is the interface for performming healthchecks against the Datastore.
@@ -29,23 +26,59 @@ type HealthChecker interface {
 
 // RedisDB is the redis implementation of the Datastore interface.
 type RedisDB struct {
-	p *pool.Pool
+	p    *pool.Pool
+	keep int
 }
 
 // NewRedisBackend creates an instance of RedisDB.
-func NewRedisBackend(u *url.URL, size int) (*RedisDB, error) {
+func NewRedisBackend(u *url.URL, keep, size int) (*RedisDB, error) {
 
 	client, err := pool.NewCustom("tcp", u.Host, size, dialer(u.User))
 	if err != nil {
 		return nil, err
 	}
 
-	return &RedisDB{client}, nil
+	db := &RedisDB{
+		p:    client,
+		keep: keep,
+	}
+
+	return db, nil
 }
 
 // Insert inserts a batch into redis.
-func (db *RedisDB) Insert(batch Batch) {
+func (db *RedisDB) Insert(token string, lines []string) (int, error) {
+	conn, err := db.p.Get()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"at":  "Insert",
+			"err": err,
+		}).Error()
+		return 0, err
+	}
+	defer db.p.Put(conn)
 
+	if err := conn.Cmd("LPUSH", token, lines).Err; err != nil {
+		log.WithFields(log.Fields{
+			"at":  "Insert",
+			"err": err,
+		}).Error()
+		return 0, err
+	}
+
+	if err := conn.Cmd("LTRIM", token, 0, db.keep).Err; err != nil {
+		log.WithFields(log.Fields{
+			"at":  "Insert",
+			"err": err,
+		}).Error()
+		return len(lines), err
+	}
+
+	log.WithFields(log.Fields{
+		"at":    "Insert",
+		"lines": len(lines),
+	}).Info("successfully stored logs in redis")
+	return len(lines), nil
 }
 
 // Healthcheck performs a PING against redis.
